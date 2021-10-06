ec5ebeca4d784e728e0a830028e8f3cb637259de
nemesxv <lupenkov14@gmail.com>
2021-10-06 16:23:14 +0200

    test de cookies

--- juego.php
+++ juego.php
@@ -37,10 +37,10 @@ setcookie($ancho, $largo, $tabla, time() + (86400 * 30));
         echo '<div class="center">';?>
     <?php
 if(!isset($_COOKIE[$cookie_name])) {
-  echo "Cookie named '" . $cookie_name . "' is not set!";
+  echo "Cookie named '" . $ancho . $largo . "' is not set!";
 } else {
-  echo "Cookie '" . $cookie_name . "' is set!<br>";
-  echo "Value is: " . $_COOKIE[$cookie_name];
+  echo "Cookie '" . $ancho . "' is set!<br>";
+  echo "Value is: " . $_COOKIE[$largo];
 }
 ?>
 </body>
